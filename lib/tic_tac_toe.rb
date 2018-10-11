b3739c46e6cdf26a5b5dace87936fbdc45899436
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2018-10-11 09:29:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,3 +71,13 @@ def turn(board)
     turn(board)
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+    if(board[win[0]] == board[win[1]] && board[win[0]] == board[win[2]] && position_taken?(board, win[0]))
+      return win
+    end
+  end
+
+  return false
+end
